9ff4e623063f0ad7f3b98595c7870dd37cf6affa
John Lukenoff <jlukenoff@gmail.com>
2018-07-20 11:31:50 -0700

    initializes test suite

--- __test__/setup.js
+++ __test__/setup.js
@@ -3,3 +3,7 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+test('1 + 2 should equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
