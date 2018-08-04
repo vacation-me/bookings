ba3bc14eedf5ebd1ecfeecd0b3d605f98e8ccb29
John Lukenoff <jlukenoff@gmail.com>
2018-08-04 10:55:03 -0700

    Fix calendar title test

--- client/components/Calendar/CalendarTitle.test.jsx
+++ client/components/Calendar/CalendarTitle.test.jsx
@@ -5,10 +5,19 @@ import CalendarTitle from './CalendarTitle';
 describe('CalendarTitle', () => {
   const renderTitleMock = (which) => {
     if (which === 0) {
-      return <p id="in">check-in</p>;
+      return (
+        <p id="in">
+          check-in
+        </p>
+      );
     }
-    return <p id="out">check-out</p>;
-  } 
+    return (
+      <p id="out">
+        check-out
+      </p>
+    );
+  };
+
   it('renders correctly', () => {
     const wrapper = shallow(<CalendarTitle renderTitle={renderTitleMock} icons={{ checkInArrow: '' }} />);
     const checkIn = wrapper.find('#in');
